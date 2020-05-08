0e1cf3001caf82f8077c8626c53c10430bf8f3f7
radoxpi <samuelmat19@gmail.com>
2020-05-09 01:07:19 +0200

    Last layer gan

--- common_definitions.py
+++ common_definitions.py
@@ -51,7 +51,7 @@ LEARNING_RATE = 1e-4
 # ACTIVIY_REGULARIZER_VAL = 1e-3  # TODO: check this value out
 
 # for training domAdap
-LAMBDA_ADV = 1.  # .001
+LAMBDA_ADV = .001  # .001
 LABEL_SMOOTHING = .1
 
 # eval settings
